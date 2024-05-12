d3b757999cbfb120b3df2240e73218f298a5603e
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-12 00:03:33 -0700

    initial setup

---
+++ src/main/java/com/presta/soapweb/SoapwebApplication.java
@@ -0,0 +1,13 @@
+package com.presta.soapweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/presta/soapweb/SoapwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.presta.soapweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
