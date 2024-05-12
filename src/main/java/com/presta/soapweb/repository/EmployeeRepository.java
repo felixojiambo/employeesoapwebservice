cf83b24441670bb9fd5322b551aa9e0f84661035
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-12 04:45:22 -0700

    repository employee

---
+++ src/main/java/com/presta/soapweb/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.presta.soapweb.repository;
+
+import com.presta.soapweb.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee , Long> {
+  Employee findByEmployeeId(Long employeeId);
+}
